392103878371f558345f9f349b3713d9d747e9ef
Robin Appelman <icewind@owncloud.com>
2015-03-29 11:21:10 +0200

    optimize builds

--- squish/build.sh
+++ squish/build.sh
@@ -1,12 +1,24 @@
 #! /usr/bin/bash
 
-emcc -o ../src/squish.js ./*.cpp -s EXPORTED_FUNCTIONS="['_GetStorageRequirements','_CompressImage','_DecompressImage']"
+emcc -O2 -o ../src/squish.js ./*.cpp --memory-init-file 0 -s EXPORTED_FUNCTIONS="['_GetStorageRequirements','_CompressImage','_DecompressImage']"
+emcc -O2 -o ../squish.bc ./*.cpp -s EXPORTED_FUNCTIONS="['_GetStorageRequirements','_CompressImage','_DecompressImage']"
 
-#// The environment setup code below is customized to use Module.
-#// *** Environment setup code ***
+# make the result work with browserify
+
+MATCH='var ENVIRONMENT_IS_NODE=typeof process==="object"&&typeof require==="function";var ENVIRONMENT_IS_WEB=typeof window==="object";var ENVIRONMENT_IS_WORKER=typeof importScripts==="function";'
+
+REPLACE='ENVIRONMENT_IS_WEB=typeof window==="object";var ENVIRONMENT_IS_WORKER=typeof importScripts==="function";var ENVIRONMENT_IS_NODE=typeof process==="object"&&typeof require==="function"&&!ENVIRONMENT_IS_WEB&&!ENVIRONMENT_IS_WORKER;if(typeof module!=="undefined"){module["exports"] = Module;}'
+
+MATCH=$(sed -e 's/[]\/$*.^|[]/\\&/g' <<< $MATCH)
+REPLACE=$(sed -e 's/[\/&]/\\&/g' <<< $REPLACE)
+
+sed -i "s/$MATCH/$REPLACE/" ../src/squish.js
+
+# The header we want
+
 #var ENVIRONMENT_IS_WEB = typeof window === 'object';
-#var ENVIRONMENT_IS_NODE = (typeof process === 'object' && typeof require === 'function') && !ENVIRONMENT_IS_WEB;
 #var ENVIRONMENT_IS_WORKER = typeof importScripts === 'function';
+#var ENVIRONMENT_IS_NODE = (typeof process === 'object' && typeof require === 'function') && !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_WORKER;
 #var ENVIRONMENT_IS_SHELL = !ENVIRONMENT_IS_WEB && !ENVIRONMENT_IS_NODE && !ENVIRONMENT_IS_WORKER;
 #
 #if (typeof module !== 'undefined') {
